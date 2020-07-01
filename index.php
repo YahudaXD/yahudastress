3b40d5f6cd33d5d458356e891cf66c0132ca2cfc
deaglebey <33934339+deaglebey@users.noreply.github.com>
2020-07-01 22:03:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<meta name="author" content="">
+
+	<title>5 Gbit/s Free Ddos</title>
+</head>
+<!-- PHP DOS, coded by EXE -->
+<style type="text/css">
+
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	font-style: normal;
+	line-height: normal;
+	color: #FFFFFF;
+	background-color: #000000;
+	 background-image: url("https://46.media.tumblr.com/886224a662107f056e4e739655f65427/tumblr_o7w2veChcr1runoqyo9_r1_540.gif");
+
+}
+
+
+
+</style>
+<!-- PHP DOS, coded by EXE -->
+<body>
+<center><b>Created by Yahuda</b><br><br>
+<img src="https://steamuserimages-a.akamaihd.net/ugc/200805689904018335/DBE9066FE242CF18CBFB9812646618E0019BD444/"><br>
+<font color="red">Yahuda Travis god</font><br><br>
+<form name="input" action="function.php" method="post">
+IP:
+<input type="text" name="ip" size="15" maxlength="15" class="main" value = "0.0.0.0" onblur = "if ( this.value=='' ) this.value = '0.0.0.0';" onfocus = " if ( this.value == '0.0.0.0' ) this.value = '';">
+&nbsp;&nbsp;&nbsp;&nbsp;Zaman:
+<input type="text" name="time" size="14" maxlength="20" class="main" value = "Zaman (Saniye olarak)" onblur = "if ( this.value=='' ) this.value = 'time (in seconds)';" onfocus = " if ( this.value == 'time (in seconds)' ) this.value = '';">
+&nbsp;&nbsp;&nbsp;&nbsp;Port:
+<input type="text" name="port" size="5" maxlength="5" class="main" value = "port" onblur = "if ( this.value=='' ) this.value = 'port';" onfocus = " if ( this.value == 'port' ) this.value = '';">
+<br><br>
+<input type="submit" value="    Saldırıyı Başlat    ">
+<br><br>
+<center>
+DoS saldırısını başlattıktan sonra, lütfen tarayıcı yüklenirken bekleyin. (Tarayıcı yükleniyorsa saldırı atılıyor demektir)
+</center>
+
+</form>
+</center>
+<!-- PHP DOS, coded by EXE -->
+</body>
+</html>
